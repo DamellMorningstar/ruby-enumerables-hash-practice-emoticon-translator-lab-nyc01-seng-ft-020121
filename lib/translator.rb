4e515df202c5f166003d910c4410688e1960e50a
Da-mell Faines <damellfaines8@gmail.com>
2021-01-24 18:46:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,11 @@ def load_library(path)
   # code goes here
   emoticons = YAML.load_file(path)
   translator = {}
+  emoticons.each do |key,value|
+    translator[key] = {}
+    translator[key][:english] = value[0]
+    translator[key][:Japanese] = value[1]
+
 
 end
 
