80c124c3634ef22f6182fe273d8ddbb98a1d7417
Da-mell Faines <damellfaines8@gmail.com>
2021-01-24 18:41:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
-# require modules here
+# require modules
+require "yaml"
+require "pry"
 
-def load_library
+def load_library(path)
   # code goes here
+  emoticons = YAML.load_file(path)
+  translator = {}
+
 end
 
 def get_japanese_emoticon
